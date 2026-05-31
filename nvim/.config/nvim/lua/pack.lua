8e5b650b8dd41c45ef9657a451034132bb218ebe
x-wang-x <69587456+x-wang-x@users.noreply.github.com>
2026-06-01 00:21:49 +0700

    add treesitter

--- nvim/.config/nvim/lua/pack.lua
+++ nvim/.config/nvim/lua/pack.lua
@@ -16,73 +16,12 @@ vim.pack.add {
     'https://github.com/nvim-mini/mini.surround',
     --notify
     'https://github.com/nvim-mini/mini.notify',
+    --treesitter
+    "https://github.com/nvim-treesitter/nvim-treesitter"
 }
-
--- local telescope = require("telescope")
--- local actions = require("telescope.actions")
--- local action_state = require("telescope.actions.state")
--- -- Delete buffer
--- local d_buffer = function(prompt_bufnr)
--- 	local selection = action_state.get_selected_entry()
--- 	if selection and selection.bufnr then
--- 		actions.delete_buffer(prompt_bufnr)
--- 	else
--- 		print("Not an active buffer.")
--- 	end
--- end
--- -- Delete file
--- local hard_d = function(prompt_bufnr)
--- 	local selection = action_state.get_selected_entry()
--- 	local filename = selection.path or selection.filename
--- 	if not filename then
--- 		return
--- 	end
--- 	-- Confirmation
--- 	vim.ui.input({ prompt = "Delete " .. filename .. " from disk? (y/n): " }, function(input)
--- 		if input == "y" or input == "Y" then
--- 			if selection.bufnr then
--- 				actions.delete_buffer(prompt_bufnr)
--- 			end
--- 			os.remove(filename)
--- 			print("Deleted: " .. filename)
--- 		end
--- 	end)
--- 	actions.close(prompt_bufnr)
--- end
---
--- -- Run the setup using the variables defined above
--- telescope.setup({
--- 	defaults = {
--- 		winblend = 0,
--- 		mappings = {
--- 			i = {
--- 				["<C-d>"] = d_buffer,
--- 				["<C-x>"] = hard_d,
--- 				["<C-n>"] = function(prompt_bufnr)
--- 					actions.close(prompt_bufnr)
--- 					vim.ui.input({ prompt = "New file: " }, function(input)
--- 						if input and input ~= "" then
--- 							vim.cmd("edit " .. input)
--- 						end
--- 					end)
--- 				end,
--- 				["QQ"] = function(prompt_bufnr)
--- 					actions.close(prompt_bufnr)
--- 				end,
--- 			},
--- 		},
--- 	},
--- 	pickers = {
--- 		buffers = {
--- 			hidden = true,
--- 			show_all_buffers = true,
--- 			sort_lastused = true,
--- 			theme = "dropdown",
--- 			previewer = false,
--- 		},
--- 	},
--- })
-
+require('nvim-treesitter').setup {
+  install_dir = vim.fn.stdpath('data') .. '/site'
+}
 require("oil").setup({
 	columns = {
 		"icon",
@@ -117,6 +56,6 @@ mini_notify.setup()
 -- })
 require("lualine").setup({
 	options = {
-		theme = "carbonfox", 
+		theme = "catppuccin-frappe", 
 	},
 })
