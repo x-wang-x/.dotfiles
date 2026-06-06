5dbe3d018ff8cc17d3ee867b3dff7c8ee136efd5
x-wang-x <69587456+x-wang-x@users.noreply.github.com>
2026-06-06 16:36:24 +0700

    remove pipewire cz its enabled in system daemon

--- hypr/.config/hypr/modules/autostart.lua
+++ hypr/.config/hypr/modules/autostart.lua
@@ -3,6 +3,5 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("nm-applet") -- nework manager
     hl.exec_cmd("awww-daemon") -- wallpaper
     hl.exec_cmd("waybar &") -- status bar
-    hl.exec_cmd("pipewire") -- audio system
-    hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+    hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP") -- tell systemd to use modern wayland session
 end)
